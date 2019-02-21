888cb9b2c10673cb50d9b0e36f587c8d08fd00a0
Gisele <42362130+giseleml@users.noreply.github.com>
2019-02-21 13:29:50 -0300

    its only right

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// let suco = ['uva', 'morango', 'manga', 'maracuja', 'limão']
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// const Liquidificador = (arr) => {
+  return arr
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// Liquidificador(suco[1])
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+// function Bolo (arr, num) {
+    return arr[num]
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+// let numeros = [1, '2', !3, false, true]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+// Bolo(numeros)
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +51,38 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+// 
+  function book (name) {
+    let receiver = {
+      AliceinWonderland: {
+        quantidadePaginas: 80,
+        autor: 'Lewis Carroll',
+        editora: 'Mass'
+      },
+      SophiesVerden: {
+        quantidadePaginas: 389,
+        autor: 'Jostein Gaarder',
+        editora: 'Cia das Letras'
+    },
+      UmbrellaAcademy: {
+        quantidadePaginas: 28,
+        autor: 'Gerard Way',
+        editora: 'Dark Horse Comics'
+    }
+      return receiver(name)
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+// ? book(receiver)
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+// ? `O livro ${book(umbrellaAcademy) tem ${book(UmbrellaAcademy.quantidadePaginas} páginas!`}
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
